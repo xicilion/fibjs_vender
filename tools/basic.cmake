1fa46af32a5cc2dc3f4c5e0f93f03e82ebb7c991
xiao <xruizhi@qq.com>
2019-03-20 18:08:53 +0800

    fiishi clang cross-compile

--- tools/basic.cmake
+++ tools/basic.cmake
@@ -12,7 +12,6 @@ endif(CCACHE_FOUND)
 file(GLOB_RECURSE src_list "src/*.c*")
 add_library(${name} ${src_list})
 
-message("bbbbbbbbbbbbbbbbbbbbb====${ARCH}${ARCH}${ARCH}${ARCH}")
 SET(LIBRARY_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/../../bin/${OS}_${ARCH}_${BUILD_TYPE})
 
 if(NOT flags)
