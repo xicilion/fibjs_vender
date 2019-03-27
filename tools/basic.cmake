d37df7575bb9e1bc7067c130aa3e587578ba70c7
xiao <xruizhi@qq.com>
2019-03-27 11:13:01 +0800

    删除多余的回车空格

--- tools/basic.cmake
+++ tools/basic.cmake
@@ -11,6 +11,7 @@ endif(CCACHE_FOUND)
 
 file(GLOB_RECURSE src_list "src/*.c*")
 add_library(${name} ${src_list})
+
 SET(LIBRARY_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/../../bin/${OS}_${ARCH}_${BUILD_TYPE})
 
 if(NOT flags)
